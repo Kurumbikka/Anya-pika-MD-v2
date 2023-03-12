2149812c38f64e989385e5d7229e200804ff2c01
Kurumbikka <104162532+Kurumbikka@users.noreply.github.com>
2023-03-12 19:48:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,15 +12,15 @@ global.headerText = "Anya by Pika" // text for header of menu
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
+global.ownername = "Kurumb ikka"   // Owner name //        
+global.ownernumber = ['917902627801'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.state = "Kerala" // your state name
+global.district = "Pathanamthitta" // your district name
+global.timezone = "Asia/kerala" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
